19cffba04575f89420e5c3a5f2140bda2875d203
rbalink <robert.balink2@gmail.com>
2021-05-05 19:05:19 +0200

    bash command test

--- src/Performance.java
+++ src/Performance.java
@@ -1,9 +1,42 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class Performance {
 
 	public static void main(String[] args) {
 		System.out.println("Hello VM-Machine");
-
+		
+		try {
+			
+			//TODO: check if Linux --- else exit
+			
+			ProcessBuilder builder = new ProcessBuilder();
+			StringBuilder out = new StringBuilder();
+			String text;
+		
+			//test command
+			builder.command("bash", "-c", "ls /home/");
+		
+			//start process
+			Process process = builder.start();
+			
+			//read terminal
+			BufferedReader read = new BufferedReader(new InputStreamReader(process.getInputStream()));
+			
+			while((text = read.readLine()) != null){
+				out.append(text + "\n");
+			}
+			
+			//finish process
+			//TODO: else cases
+			int exit = process.waitFor();
+			System.out.println("\nFinished : " + exit);
+			
+			
+		} catch (Exception e){
+			e.printStackTrace();
+		}
+		//TODO: more Exceptions
 	}
 
 }
