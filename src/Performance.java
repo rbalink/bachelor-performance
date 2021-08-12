a6085c9a724b91b41434ac3a5aa3cf810a7720a1
rbalink <robert.balink@gmail.com>
2021-08-12 13:14:54 -0400

    refactor + first scrape

--- src/Performance.java
+++ src/Performance.java
@@ -4,72 +4,129 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.Statement;
 import java.util.HashMap;
+import java.util.logging.Level;
+
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
 
 public class Performance {
+	final String url = "jdbc:postgresql://35.234.64.171:5432/performance";
+	final String user = "postgres";
+	final String password = "geheim";
+	HashMap<String, String> lscpu;
+	CPU prozessor;
+
+	public Performance() {
+		this.lscpu = new HashMap<>();
+	}
 
 	public static void main(String[] args) {
-		final String url = "jdbc:postgresql://35.234.107.124:5432/";
-		final String user = "postgres";
-		final String password = "geheim";
-		HashMap<String, String> lscpu = new HashMap<>();
+		Performance performance = new Performance();
+		performance.getHardwareInformation();
+
+		// TODO: what if terminal language is english?
 		
+		// online CPU Database (boinc rosetta GFLOPS)
+		performance.scrapingDatabase();
+
+		// external postgres database
+		performance.externalDatabase();
+	}
+
+	public void getHardwareInformation() {
 		try {
-			
-			if(!(System.getProperty("os.name").equals("Linux"))){
+
+			if (!(System.getProperty("os.name").equals("Linux"))) {
 				System.err.println("The operating system for this program should be Linux!");
+				throw new Exception();
 			}
-			
-			
+
 			ProcessBuilder builder = new ProcessBuilder();
 			String text;
-			
-			//list cpu info
+
+			// list cpu info
 			builder.command("bash", "-c", "lscpu");
-		
-			//start process
+
+			// read from terminal
 			Process process = builder.start();
-			
-			//read terminal
 			BufferedReader read = new BufferedReader(new InputStreamReader(process.getInputStream()));
-			
-			while((text = read.readLine()) != null){
+
+			while ((text = read.readLine()) != null) {
 				String[] parts = text.split(":", 2);
 				parts[1] = parts[1].trim();
-				lscpu.put(parts[0], parts[1]);
+				this.lscpu.put(parts[0], parts[1]);
 			}
+			
 			int exit = process.waitFor();
-			
-			//TODO: what if terminal language is english?
-			String modelname = lscpu.get("Modellname");
-			String model = lscpu.get("Modell");
-			int stepping = Integer.parseInt(lscpu.get("Stepping"));
-			float cpumhz = Float.parseFloat(lscpu.get("CPU MHz"));
-			int cpufamily = Integer.parseInt(lscpu.get("Prozessorfamilie"));
-			String l1dcache = "";
-			String l1icache = "";
-			String l2cache = "";
-			String l3cache = "";
-			
-			//printout hashmap
-			//lscpu.entrySet().forEach(entry -> {System.out.println(entry.getKey() + " "+ entry.getValue());
-			//});
-		
-			//TODO: Connection to Online-CPU-Database (cpu-world.com)
-		
-			// Connection to remote PostgreSQL database
+
+		} catch (Exception e) {
+			System.err.println("Hardware Error");
+			e.printStackTrace();
+		}
+
+		prozessor = new CPU(lscpu.get("Modellname"), lscpu.get("Modell"), Integer.parseInt(lscpu.get("Stepping")),
+				Float.parseFloat(lscpu.get("CPU MHz")), Integer.parseInt(lscpu.get("Prozessorfamilie")), "Test", "", "",
+				"");
+	}
+
+	public void externalDatabase() {
+		try {
 			System.out.println("Loading the Driver");
 			Class.forName("org.postgresql.Driver");
 			System.out.println("Connecting to external PostgreSQL Server");
-			Connection con = DriverManager.getConnection(url,user,password);
+			Connection con = DriverManager.getConnection(this.url, this.user, this.password);
+			System.out.println(con.getSchema());
+
 			System.out.println("Successful connection");
-			
+
 			Statement statement = con.createStatement();
-			statement.execute("INSERT INTO datatbl (cpu_model_name, cpu_family, cpu_model, stepping, cpu_mhz)"+ "VALUES ('"+modelname+"',"+ cpufamily+","+ model+","+ stepping+","+cpumhz+");");
-			
+			statement.execute("INSERT INTO postgres (cpu_model_name, cpu_family, cpu_model, stepping, cpu_mhz)"
+					+ "VALUES ('" + this.prozessor.modelname + "'," + this.prozessor.cpufamily + ","
+					+ this.prozessor.model + "," + this.prozessor.stepping + "," + this.prozessor.cpumhz + ");");
+
 			System.out.println("Inserting successful");
-		} catch ( Exception e){
+		} catch (Exception e) {
+			System.err.println("PostgresQL Error");
 			e.printStackTrace();
 		}
 	}
 
+	public void scrapingDatabase() {
+		java.util.logging.Logger.getLogger("com.gargoylesoftware").setLevel(Level.OFF);
+
+		try {
+			final WebClient webClient = new WebClient();
+			final HtmlPage page = webClient.getPage("https://boinc.bakerlab.org/rosetta/cpu_list.php");
+			System.out.println("Website: " + page.getTitleText());
+		} catch (Exception e) {
+			System.err.println("htmlunit Error");
+			e.printStackTrace();
+		}
+	}
+
+}
+
+class CPU {
+	String modelname;
+	String model;
+	int stepping;
+	float cpumhz;
+	int cpufamily;
+	String l1dcache;
+	String l1icache;
+	String l2cache;
+	String l3cache;
+
+	public CPU(String modelname, String model, int stepping, float cpumhz, int cpufamily, String l1dcache,
+			String l1icache, String l2cache, String l3cache) {
+		this.modelname = modelname;
+		this.model = model;
+		this.stepping = stepping;
+		this.cpumhz = cpumhz;
+		this.cpufamily = cpufamily;
+		this.l1dcache = l1dcache;
+		this.l1icache = l1icache;
+		this.l2cache = l2cache;
+		this.l3cache = l3cache;
+	}
 }
